20f3e868b2e84864bdf672c912750135f2ed4094
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-20 04:30:29 +0300

    Add project files.

---
+++ OOP Asssignment_2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOP_Asssignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+
+            #endregion
+        }
+    }
+}
