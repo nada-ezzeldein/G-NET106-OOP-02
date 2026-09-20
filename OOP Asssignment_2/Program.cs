7dcf1c2f43ea05005967f02ce3a6f6dd3574b7b0
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-20 04:35:33 +0300

    Question 1

--- OOP Asssignment_2/Program.cs
+++ OOP Asssignment_2/Program.cs
@@ -5,8 +5,18 @@
         static void Main(string[] args)
         {
             #region Question 1
+            // What is the difference between a class and a struct?
+            // A class is a reference type, while a struct is a value type. 
+            // Classes are allocated on the heap, while structs are allocated on the stack.
+            // classes are private by default, while structs are public by default.
 
+            // Why are classes more suitable than structs for large applications?
+            // Because they support inheritance, polymorphism, and encapsulation
             #endregion
+
+
+
+
         }
     }
 }
